58a01c4a4cd143b333bb0bbe22086ed602fc34b4
Tim Quimbo <tquimbo1@gmail.com>
2021-02-25 23:59:13 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   if board[index] == true
-  elsif board[" "] == false
+  elsif board[" "] or board[""] or nil == false
   end
 end
