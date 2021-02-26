fe18b47febebd2ab72964bcca32497511c4a5643
Tim Quimbo <tquimbo1@gmail.com>
2021-02-26 00:46:32 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if board[index] == `" "` || board[index] == `""` || board[index] == nil
     return false
-  elsif board[index] == `X` || `O`
-   return true
+  # elsif board[index] == `X` || `O`
+  #  return true
   end
 end
