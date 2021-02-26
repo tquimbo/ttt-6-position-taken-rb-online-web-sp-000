09e0fded9b366d069be36cb58b0285da1498ca67
Tim Quimbo <tquimbo1@gmail.com>
2021-02-26 00:30:23 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] == `X` || `O`
-  true
-  elsif board[index] == `" "` || `""`
+  if board[index] == `" "` || `""`
     false
+  # elsif board[index] == `X` || O
+  #   true
   end
 end
